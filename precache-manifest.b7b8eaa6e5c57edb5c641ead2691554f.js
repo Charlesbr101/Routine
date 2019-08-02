5a6fbfde25cf9ec0716b98d94ae72fa4c41c1390
Charlesbr101 <51233653+Charlesbr101@users.noreply.github.com>
2019-08-02 11:23:03 -0300

    Add files via upload

---
+++ precache-manifest.b7b8eaa6e5c57edb5c641ead2691554f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1510c90f90f6993a8e12fbcf87b103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d73ba597be044fd8339",
+    "url": "/static/css/2.074fc065.chunk.css"
+  },
+  {
+    "revision": "21d8233e6d3d7fcaaaf4",
+    "url": "/static/css/main.f4fab485.chunk.css"
+  },
+  {
+    "revision": "9d73ba597be044fd8339",
+    "url": "/static/js/2.9ff1e924.chunk.js"
+  },
+  {
+    "revision": "21d8233e6d3d7fcaaaf4",
+    "url": "/static/js/main.d0536a7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
